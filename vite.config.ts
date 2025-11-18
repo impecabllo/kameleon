8009af9ce9320deef63ddc0f4162222b8230b454
Dmitriy Karzakov <dk@didlogic.com>
2025-11-18 22:10:41 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kameleon/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
